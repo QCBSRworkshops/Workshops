06838355b318b01b83ee40c8bbe0c43691f137d7
mhBrice <mariehbrice@outlook.com>
2019-02-15 13:45:11 -0500

    remove package in w3

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ wget https://github.com/QCBSRworkshops/templateWorkshops/archive/master.zip
 unzip master
 
 
-for i in 1 2 3 4 5 6 8 10
+for i in 1 2 3 4 6 7 8 10
 do
   num=$(printf "%02d" $i)
   # files in folder
